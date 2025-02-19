14f296213cb52f320f8191d7c865fdd8ec71e634
Dan Folescu <dan134535334@gmail.com>
2025-02-19 17:43:20 -0500

    added some damped wave disc. a la embree & cox. still need to translate to the fn for a(x) via approx eigs

--- experiments/modal_truncation/ex3.m
+++ experiments/modal_truncation/ex3.m
@@ -1,23 +1,35 @@
-%% construct fn in tf and pole-residue form
-n = 100; d = 10; m = n; p = n;
-ewref = (d*(randn(n,1) + 1i*randn(n,1)));
-[~,ewsidx] = sort(abs(ewref)); ewref = ewref(ewsidx);
-A = diag(ewref);
+%% chebfun example code
+n = 200; d = [0,1];
+
+x = chebpts(n,d); L = diffmat(n,2,'dirichlet','dirichlet');
+
+% a = @(x) (1 - sin(pi*x));
+a = @(x) 1/2 + exp(-256*(0.5 - x).^2);
+% a = @(x) (1 - sin((11*pi)*x).*exp(-10*(x - 1/2).^2));
+
+Aref = [zeros(n) eye(n);L -2*diag(a(x))]; ewref = eig(Aref);
+
+scatter(real(ewref),imag(ewref));
+xlim([-1.3 0]); ylim([0 50]); grid;
+
+% plot(chebfun(a,[0,1],n))
+%% tf
+contour = Numerics.Contour.Circle(0,50,1e2);
+ewref = ewref(contour.inside(ewref));
 %
-B = randn(n,m); C = randn(p,n);
+n = length(ewref);
+[~,ewsidx] = sort(abs(ewref)); ewref = ewref(ewsidx);
+A = diag(ewref); B = eye(n); C = eye(n);
 %
 H = @(z) C*((z*eye(n) - A) \ B); G = @(z) ihml(z,n,ewref,B,C);
 Th = @(z) pinv(B)*(z*eye(n) - A)*pinv(C); Tg = @(z) iihml(z,n,ewref,B,C);
 w = logspace(-1,4); Nbode(w,H,G);
 %% setup CIMTOOL
 nlevp = Numerics.NLEVPData(Tg);
-contour = Numerics.Contour.Circle(0,norm(A)+d,1e2);
 CIM = Numerics.CIM(nlevp,contour);
-CIM.SampleData.show_progress = false;
 CIM.SampleData.NLEVP.refew = ewref;
 c = CIMTOOL(CIM);
 %% check initial bode
-
 nec = length(ewref(CIM.SampleData.Contour.inside(ewref))); CIM.RealizationData.m = nec;
 
 CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.Hankel;
@@ -30,37 +42,25 @@ CIM.SampleData.ell = n; CIM.SampleData.r = n; CIM.RealizationData.K = n;
 CIM.compute(); [~,V2,W2,M21,M22] = CIM.ResultData.rtf();
 Hrmpl = @(z) V2*((M21-z*M22)\W2);
 %
-close all; Nbode(w,H,Hrhnk,Hrmpl);
+close all; Nbode(w,H,Hrhnk,Hrmpl); legend('H','HrHankel','HrMPL','Location','northoutside','Orientation','horizontal');
 %% changing radius bode, fixed N
-sradius = norm(A)+d; CIM.SampleData.Contour.N = 64;
+CIM.SampleData.show_progress = false;
+sradius = norm(A)*1.1; CIM.SampleData.Contour.N = 128;
 %
-% CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.Hankel;
-% CIM.SampleData.ell = 25; CIM.SampleData.r = 25; CIM.RealizationData.K = 4;
+CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.Hankel;
+CIM.SampleData.ell = 25; CIM.SampleData.r = 25; CIM.RealizationData.K = 4;
 %
-CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.MPLoewner;
-K = n; CIM.SampleData.ell = n; CIM.SampleData.r = n; CIM.RealizationData.K = K;
+% CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.MPLoewner;
+% K = n; CIM.SampleData.ell = 25; CIM.SampleData.r = 25; CIM.RealizationData.K = K;
 
-x = linspace(sradius,5,30);
+x = linspace(sradius,1,50);
 for i=1:length(x)
     CIM.SampleData.Contour.rho = x(i);
     nec = length(ewref(CIM.SampleData.Contour.inside(ewref)));
     CIM.RealizationData.m = nec; CIM.compute();
     G = @(z) ihml(z,nec,ewref,B,C); Hr = CIM.ResultData.rtf();
-    Nbode(w,H,G,Hr); legend('H','Gr','Hr','Location','northoutside','Orientation','horizontal'); drawnow; pause(0.1)
-end
-%% changing N bode, fixed radius
-CIM.SampleData.Contour.rho = 10; CIM.SampleData.Contour.N = 8;
-% %
-CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.Hankel;
-CIM.SampleData.ell = 25; CIM.SampleData.r = 25; CIM.RealizationData.K = 4;
-% %
-% CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.MPLoewner;
-% K = n; CIM.SampleData.ell = 25; CIM.SampleData.r = 25; CIM.RealizationData.K = K;
-
-for i=25:25:300
-    CIM.SampleData.Contour.N = i;
-    nec = length(ewref(CIM.SampleData.Contour.inside(ewref)));
-    CIM.RealizationData.m = nec; CIM.compute();
-    G = @(z) ihml(z,nec,ewref,B,C); Hr = CIM.ResultData.rtf();
-    Nbode(w,H,G,Hr); legend('H','Gr','Hr','Location','northoutside','Orientation','horizontal'); drawnow; pause(0.1)
+    GHre = @(z) G(z) - Hr(z);
+    Nbode(w,H,G,Hr); legend('H','Gr','Hr','Location','northoutside','Orientation','horizontal');
+    % Nbode(w,GHre); legend('GHre','Location','northoutside','Orientation','horizontal');    
+    drawnow; pause(0.1)
 end
