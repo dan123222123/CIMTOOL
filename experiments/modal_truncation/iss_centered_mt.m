6bdd55f9bf198696fd51400c7553d752a26b8e53
Dan Folescu <dan134535334@gmail.com>
2025-03-04 13:27:08 -0500

    ready for runs

--- experiments/modal_truncation/iss_centered_mt.m
+++ experiments/modal_truncation/iss_centered_mt.m
@@ -7,13 +7,14 @@ H = @(z) full(C*((z*speye(n) - A) \ B)); w = logspace(-1,3,5000);
 
 %% setup CIM
 nlevp = Numerics.NLEVPData(H); nlevp.sample_mode = Numerics.SampleMode.Direct;
-contour = Numerics.Contour.Ellipse(-0.3067+61.3391i,0.025,1.5,5e2);
-tc =      Numerics.Contour.Ellipse(-0.3+62i,0.025,1.5,5e2);
+s = 1.2;
+contour = Numerics.Contour.Ellipse(0,2*max(abs(real(ewref)))*s,max(abs(imag(ewref)))*s,5e2);
+tc =      Numerics.Contour.Ellipse(0,2*max(abs(real(ewref)))*s,max(abs(imag(ewref)))*s,5e2);
 CIM = Numerics.CIM(nlevp,contour);
 %
 CIM.SampleData.NLEVP.refew = ewref;
 CIM.RealizationData.ComputationalMode = Numerics.ComputationalMode.MPLoewner;
-CIM.SampleData.ell = 3; CIM.SampleData.r = 3; CIM.RealizationData.K = 50;
+CIM.SampleData.ell = 3; CIM.SampleData.r = 3; CIM.RealizationData.K = 1000;
 CIM.SampleData.show_progress = false;
 
 %% CIMTOOl investigation (if necessary)
@@ -21,19 +22,20 @@ CIM.SampleData.show_progress = false;
 % xlim(CIM.MainAx,[-1.5 1.5]); ylim(CIM.MainAx,[-125 125]);
 
 %% contour conga
-g = @(x) x - 200*1i*x; gx = linspace(-0.3,0,1000); gls = g(gx);
+gx = linspace(2*max(abs(real(ewref)))*s,min(abs(real(ewref)))*s,10);
 
 f = figure(1); f.Visible = false; f.Position = [100 100 1920 1080];
 % the line along which gamma evolves
 % axes(f.Children(end)); hold on; plot(real(gls),imag(gls)); hold off;
 
-wobj = 'cc_iss_unilateral.gif'; delete(wobj);
-for i=1:length(gls)
+wobj = 'cc_iss_centered.gif'; delete(wobj);
+for i=1:length(gx)
+    clf(f);
     % test if there are ew inside the contour
-    tc.gamma = gls(i); nec = length(ewref(tc.inside(ewref)));
+    tc.alpha = gx(i); nec = length(ewref(tc.inside(ewref)));
     % compute and plot realized tf response
     if nec ~= 0
-        CIM.SampleData.Contour.gamma = gls(i); CIM.RealizationData.m = nec;
+        CIM.SampleData.Contour.alpha = gx(i); CIM.RealizationData.m = nec;
         CIM.compute(); [~,V2,W2,M21,M22] = CIM.ResultData.rtf(nec); Hrmpl = @(z) V2*((M21-z*M22)\W2);
         plot_cim_response(f,w,CIM,H,Hrmpl);
     else
@@ -63,7 +65,7 @@ function plot_cim_response(f,w,CIM,H,Hr)
     title(sprintf("Complex Plane (%d ew inside contour)",CIM.RealizationData.m));
     xlabel("$\bf{R}$",'Interpreter','latex'); ylabel("$i\bf{R}$",'Interpreter','latex');
     legend('Interpreter','latex','Location','northoutside','Orientation','horizontal')%,'NumColumns',2);
-    xlim([-0.4 0.05]); ylim([-20 80]);
+    xlim([-0.4 0.05]); ylim([-100 100]);
     %
     subplot(1,3,2);
     if ~isempty(Hr)
